a1871827f8ed96e1a1ce523fbc082ff37d2706c8
Sunil <52168181+sunilsinghx@users.noreply.github.com>
2021-12-17 19:50:20 +0530

    Delete New folder (2) directory

--- New folder (2)/fibbonaci.py
+++
@@ -1,9 +0,0 @@
-#fibbonacci 
-n1, n2 = -1,1
-sum = 0
-n = int(input("Number :"))
-for i in range(n+1):
-    sum = n1 + n2
-    print(sum)
-    n1,n2 = n2,sum #swap variable
-    
--- New folder (2)/lis-even.py
+++
@@ -1,5 +0,0 @@
-# lis even number from  1- 100
-
-for i in range (1,101):
-    if(i % 2 == 0):
-        print(i,end= " ")
